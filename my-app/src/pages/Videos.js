5f80694889fe80d30c419789302eecc344820c27
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-31 17:36:31 -0300

    concluido aula 03
    realizado todos as mudanças da aula 03

--- my-app/src/pages/Videos.js
+++ my-app/src/pages/Videos.js
@@ -1,35 +1,34 @@
-import React, {useRef, useState} from 'react'
-import "./videos.css"
+import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
+import "./videos.css";
 
 function Videos() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-    const videoRef = useRef(null)
-    const [play, setPlay] = useState(false)
-
-    function handdleStart() {
-
-        if (play) {
-            videoRef.current.pause();
-            setPlay(false);
-        } else {
-            videoRef.current.play();
-            setPlay(true);
-        }
-       
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
     }
+  }
 
   return (
-    <div className='videos'>
-        <video 
-            className='video__player'
-            ref={videoRef}
-            onClick={handdleStart}
-            loop
-            src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/brecker2.mp4?alt=media&token=b7e3ebf7-89a0-47a0-8a34-66f1176a71e3"
-        >
-        </video>
+    <div className="videos">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/brecker2.mp4?alt=media&token=b7e3ebf7-89a0-47a0-8a34-66f1176a71e3"
+      ></video>
+      {/*side bar*/}
+      <VideoFooter />
     </div>
-  )
+  );
 }
 
-export default Videos
+export default Videos;
