480346296a852a7b7168d35f1b391eff8928e947
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-03 11:28:56 -0300

    terminando curso

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,11 @@ import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
 
+  let maxHeight;
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight
+  }
+
   const [videos, setVideos] = useState([])
 
   async function getVideos(){
@@ -23,7 +28,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className='app__videos'>
         
         {videos.map((item)=> {
